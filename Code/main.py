2e58d23c8b9c99ca4366b1a45553be1d5772165b
Kaushik Jadhav <kajadhav@ncsu.edu>
2022-08-25 20:38:11 -0400

    Create main.py
    Signed-off-by: Kaushik Jadhav <kajadhav@ncsu.edu>

---
+++ Code/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+__author__ = "SE Team Project 11 - HW 0"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def increment(x):
+    return x+1
+
+def decrement(x):
+    return x-1
+
+def main():
+    print("hello world, this is the HW 0 project from Team 11")
+
+
+if __name__ == "__main__":
+    main()
