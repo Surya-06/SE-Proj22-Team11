ca5f4148f9acfd07b98a35b30fdd47279fab2550
Harshitha.Paruchuri <59336190+harshitha-ready-to-code@users.noreply.github.com>
2022-08-27 19:49:35 +0530

    Update test_sample.py
    added divide test
    
    Signed-off-by: Harshitha.Paruchuri <59336190+harshitha-ready-to-code@users.noreply.github.com>

--- test/test_sample.py
+++ test/test_sample.py
@@ -3,7 +3,7 @@ Run tests by running the following cmd in the root dir: py.test
 """
 
 import sys
-from Code.main import increment, decrement
+from Code.main import increment, decrement, divide
 
 def test_increment():
     for i in range(1, 1000):
@@ -12,4 +12,8 @@ def test_increment():
 def test_decrement():
     for i in range(1, 1000):
         assert decrement(i) == i-1
+        
+def test_divide():
+    for i in range(1, 1000):
+        assert divide(i) == i/2
 
