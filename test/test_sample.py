3c80f1b747df4df889124f94c08670ce4762f3f8
aditi-v79 <79243620+aditi-v79@users.noreply.github.com>
2022-08-28 18:48:11 +0530

    Update test_sample.py
    Signed-off-by: aditi-v79 <79243620+aditi-v79@users.noreply.github.com>

--- test/test_sample.py
+++ test/test_sample.py
@@ -3,7 +3,7 @@ Run tests by running the following cmd in the root dir: py.test
 """
 
 import sys
-from Code.main import increment, decrement, divide
+from Code.main import increment, decrement, square, divide
 
 def test_increment():
     for i in range(1, 1000):
@@ -12,6 +12,10 @@ def test_increment():
 def test_decrement():
     for i in range(1, 1000):
         assert decrement(i) == i-1
+        
+def test_square():
+    for i in range(1,1000):
+        assert square(i) == i*i
         
 def test_divide():
     for i in range(1, 1000):
