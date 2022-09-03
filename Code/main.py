da4f5d0c927b15a16ae941e0bf70817a7bf70935
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2022-09-03 16:56:39 -0400

    fixes for module structure

--- Code/main.py
+++ Code/main.py
@@ -4,11 +4,25 @@ __author__ = "SE Team Project 11 - HW 1"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-import classes.sym
+import Code.Classes.sym
+
+# BEGIN - Older test functions - keeping them for CI sanity
+def increment(x):
+    return x+1
+
+def decrement(x):
+    return x-1
+
+def square(x):
+    return x*x
+
+def divide(x):
+    return x/2
+# END - Older test functions - keeping them for CI sanity
+
 
 def main():
     print("This currently only has the Num and Sym classes, please run the test set to cover the functionality.")
-
 
 if __name__ == "__main__":
     main()
