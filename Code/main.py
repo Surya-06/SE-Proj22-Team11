8d3022e640a887abe431f224b67791a448620f7b
vemuriPradyumna <pradyumna.vemuri@gmail.com>
2022-09-04 15:52:26 -0400

    Changes made to sym class and tests

--- Code/main.py
+++ Code/main.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python3
 
-__author__ = "SE Team Project 11 - HW 1"
-__version__ = "0.1.0"
+__author__ = "SE Team Project 11 - HW 2"
+__version__ = "1.0.0"
 __license__ = "MIT"
-
-# BEGIN - Older test functions - keeping them for CI sanity
-def increment(x):
-    return x+1
-
-def decrement(x):
-    return x-1
-
-def square(x):
-    return x*x
-
-def divide(x):
-    return x/2
-# END - Older test functions - keeping them for CI sanity
-
 
 def main():
     print("This currently only has the Num and Sym classes, please run the test set to cover the functionality.")
--- test/test_sample.py
+++ test/test_sample.py
@@ -3,49 +3,29 @@ Run tests by running the following cmd in the root dir: py.test
 """
 
 import sys
-from Code.main import increment, decrement, square, divide
 from Code.sym import Sym
 
-# BEGIN - Old tests for CI sanity
+def eg_sym():
 
-def test_increment():
-    for i in range(1, 1000):
-        assert increment(i) == i+1
-
-def test_decrement():
-    for i in range(1, 1000):
-        assert decrement(i) == i-1
-        
-def test_square():
-    for i in range(1,1000):
-        assert square(i) == i*i
-        
-def test_divide():
-    for i in range(1, 1000):
-        assert divide(i) == i/2
-
-# END - Old tests for CI sanity
-
-def test_sym_basic():
-    test_symbol_map = {'a': 3, 'b': 5, 'c': 10, 'd': 1};
-    test_data_expanded = list()
-    for (key, value) in test_symbol_map.items():
-        for i in range(1, value+1):
-            test_data_expanded.append(key);
-
+    test_data = ["a","a","a","a","b","b","c"]
     column_name = 'Test Column 1';
     column_position = 1001;
-    expected_mode = 'c' # repeated 10 times according to the map above.
-    expected_entropy = 4 # 4 different items in the map above.
-    
+
+    expected_mode = 'a'
+    expected_entropy = 1.379
+    expected_freq_count = {'a': 4, 'b': 2, 'c': 1}
+
     test_num_obj = Sym(column_name, column_position);
-    for i in test_data_expanded:
+    for i in test_data:
         test_num_obj.addData(i);
 
-    assert column_name == test_num_obj.getColumnName();
-    assert column_position == test_num_obj.getColumnPosition();
-    assert expected_mode == test_num_obj.getMode();
-    assert expected_entropy == test_num_obj.getEntropy();
+    assert test_num_obj.freq_count == expected_freq_count
+    assert expected_mode == test_num_obj.mid()
+    assert expected_entropy == round(test_num_obj.div(), 3)
+
+    assert column_name == test_num_obj.getColumnName()
+    assert column_position == test_num_obj.getColumnPosition()
+
 
 
 
