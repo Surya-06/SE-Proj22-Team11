46c9068f27fa189481174761740eec23f7de5b94
kajadhav <kajadhav@ncsu.edu>
2022-09-04 16:41:06 -0400

    fix: var names in sym.py

--- test/test_eg.py
+++ test/test_eg.py
@@ -16,11 +16,11 @@ def test_eg_sym():
     expected_entropy = 1.379
     expected_freq_count = {'a': 4, 'b': 2, 'c': 1}
 
-    test_num_obj = Sym(column_name, column_position);
+    test_num_obj = Sym(column_position, column_name);
     for i in test_data:
         test_num_obj.add(i)
 
-    assert test_num_obj.freq_count == expected_freq_count
+    assert test_num_obj._has == expected_freq_count
     assert expected_mode == test_num_obj.mid()
     assert expected_entropy == round(test_num_obj.div(), 3)
 
