4a36724c3e9d8bb5f5e6bd50ce676cf045f0dba8
vemuriPradyumna <pradyumna.vemuri@gmail.com>
2022-09-04 16:10:33 -0400

    Sym test cases

--- test/test_sample.py
+++ test/test_sample.py
@@ -5,7 +5,7 @@ Run tests by running the following cmd in the root dir: py.test
 import sys
 from Code.sym import Sym
 
-def eg_sym():
+def test_eg_sym():
 
     test_data = ["a","a","a","a","b","b","c"]
     column_name = 'Test Column 1';
@@ -17,7 +17,7 @@ def eg_sym():
 
     test_num_obj = Sym(column_name, column_position);
     for i in test_data:
-        test_num_obj.addData(i);
+        test_num_obj.add(i);
 
     assert test_num_obj.freq_count == expected_freq_count
     assert expected_mode == test_num_obj.mid()
