7528bc12337eb07172127bea2fc292249c00bbe2
vemuriPradyumna <pradyumna.vemuri@gmail.com>
2022-09-04 21:52:05 -0400

    Added eg.num test case

--- test/test_eg.py
+++ test/test_eg.py
@@ -5,6 +5,7 @@ Run tests by running the following cmd in the root dir: py.test
 from Code.utils import *
 from Code.main import the
 from Code.sym import Sym
+from Code.num import Num
 
 def test_eg_sym():
 
@@ -16,17 +17,31 @@ def test_eg_sym():
     expected_entropy = 1.379
     expected_freq_count = {'a': 4, 'b': 2, 'c': 1}
 
-    test_num_obj = Sym(column_position, column_name);
+    test_sym_obj = Sym(column_position, column_name);
     for i in test_data:
-        test_num_obj.add(i)
+        test_sym_obj.add(i)
 
-    assert test_num_obj._has == expected_freq_count
-    assert expected_mode == test_num_obj.mid()
-    assert expected_entropy == round(test_num_obj.div(), 3)
+    assert test_sym_obj._has == expected_freq_count
+    assert expected_mode == test_sym_obj.mid()
+    assert expected_entropy == round(test_sym_obj.div(), 3)
 
-    assert column_name == test_num_obj.getColumnName()
-    assert column_position == test_num_obj.getColumnPosition()
+    assert column_name == test_sym_obj.getColumnName()
+    assert column_position == test_sym_obj.getColumnPosition()
 
 def test_eg_the():
     oo(the)
     return True
+
+def test_eg_num():
+    test_num_obj = Num()
+
+    for i in range(1, 101):
+        test_num_obj.add(i)
+
+    mid = test_num_obj.mid()
+    div = test_num_obj.div()
+
+    assert( mid>=50 and mid<=52)
+    assert( div>30.5 and div<32)     
+
+
