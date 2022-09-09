85b4dc3b0871fa10891786765189c62c625f8da3
aditi-v79 <79243620+aditi-v79@users.noreply.github.com>
2022-09-08 22:05:04 -0400

    Create setup.py
    Signed-off-by: aditi-v79 <79243620+aditi-v79@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+  name="Software Engineering Project",
+  description="CSC 510 Homework 1",
+  version="1.0",
+  packages=[''],
+  liscense='MIT',
+  author='Surya,Kaushik,Pradhyumna,Harshitha,Aditi',
+  url="https://github.com/Surya-06/SE-Proj22-Team11/blob/main/README.md"
+)
